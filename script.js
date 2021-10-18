e9b012d1ec915bf2f8c9de0c8c3dfbde078cb978
Mario Merlo <m20_11@icloud.com>
2021-10-18 09:09:36 +0200

    Removed console.log debug statements

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function sponge() {
     const resHeading = document.getElementById("result");
     let str = document.getElementById("string").value;
-    console.log(str);
 
     str = str.toLowerCase(); //Converts string to lowercase
-    console.log("lowerCase OK");
 
     for (let i = 0; i < str.length; i++) { //Spongification
         rand = getRandomInt(2);
@@ -14,7 +12,6 @@ function sponge() {
             str = str.replace(subStr, subStr.toUpperCase());
         }
     }
-    console.log("for OK");
 
     resHeading.innerHTML = "The spongified string is: " + str;
     resHeading.style.visibility = "visible";
